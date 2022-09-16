61c6c7ace06fbaf4cc79817c0c1035eef5abaa95
SamuelUkachukwu <shybold@yahoo.com>
2022-09-16 19:46:39 +0000

    Added product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Product, Category, ProductFeatures
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
+    search_fields = ('sku', 'name', 'category')
+    list_filter = ('sku', 'category', 'price')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+    search_fields = ('friendly_name',)
+
+
+class ProductFeaturesAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+    )
+    search_fields = ('product',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductFeatures, ProductFeaturesAdmin)
---
+++ products/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductFeatures(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE)
+    featur_1 = models.TextField(max_length=100, null=True, blank=True)
+    featur_2 = models.TextField(max_length=100, null=True, blank=True)
+    featur_3 = models.TextField(max_length=100, null=True, blank=True)
+    featur_4 = models.TextField(max_length=100, null=True, blank=True)
+    featur_5 = models.TextField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.product}"
