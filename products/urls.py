ba52c767ed96018c3d9016e78482b53b15499a24
SamuelUkachukwu <shybold@yahoo.com>
2022-09-17 16:00:24 +0000

    Added product fixture files. fixed product detail page not loading

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<int:product_id>/', views.product_detail_view, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Product
 
 
@@ -11,3 +11,13 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail_view(request, product_id):
+    """ Renders a detail view of the product"""
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
