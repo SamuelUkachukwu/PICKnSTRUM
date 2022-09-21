699ba45525c39f9f5dae89c1c8b103fa75c3ccec
SamuelUkachukwu <shybold@yahoo.com>
2022-09-21 12:38:55 +0000

    Added cart content update and delete functions

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 # Create your views here.
@@ -19,4 +19,36 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
+    print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """update the quantity of items in cart"""
+    # product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    print('THIS IS:', quantity)
+
+    if item_id in list(cart.keys()):
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+    cart = request.session.get('cart', {})
+    try:
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
+
+    except Exception as e:
+        print('THIS IS:', e)
+        return HttpResponse(status=500)
