a27fa2128d6a9f161f8fe0e04970b6f3819d1647
SamuelUkachukwu <shybold@yahoo.com>
2022-09-23 22:42:00 +0000

    Added checkout form and checkout page elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .forms import OrderForm
+from django.contrib import messages
 
 
 # Create your views here.
 def checkout(request):
     """Renders the checkout page"""
-    form = OrderForm()
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Your Shopping Cart is currently Empty")
+        return redirect(reverse('products'))
+    order_form = OrderForm()
     context = {
-        'form': form
+        'order_form': order_form
     }
     return render(request, 'checkout/checkout.html', context)
--- picknstrum/settings.py
+++ picknstrum/settings.py
@@ -81,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
