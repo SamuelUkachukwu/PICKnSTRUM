a8a4757ee3bb1e3325d2b51fa9dd9b56ad4c872c
SamuelUkachukwu <shybold@yahoo.com>
2022-09-24 11:30:42 +0000

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
         return redirect(reverse('products'))
     order_form = OrderForm()
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'publishable_key': 'pk_test_51LcIJvIQDXjp8otbGZdOBifdz7sUeNxwfMkjkzndAxjjxOi5LZc9fsD9Uu5wSqBwd9OFIg35Zvr0APR2hg7e877X00LSVa7j7O',
+        'client_secret': 'client secret yeah long'
     }
     return render(request, 'checkout/checkout.html', context)
