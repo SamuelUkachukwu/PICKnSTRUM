91b2b7548bd6d7f360f378e5cc8e0210001b8866
SamuelUkachukwu <shybold@yahoo.com>
2022-09-24 13:25:37 +0000

    Added checkout funtionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,4 +1,4 @@
-var stripePublicKey = $('#id_publishable_key').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
@@ -21,3 +21,44 @@ var style = {
 
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime error on card
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="me-1" role="alert"><i class="fas fa-exclamation-triangle"></i></span>
+            <span>${event.error.message}</span>`;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submission
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="me-1" role="alert"><i class="fas fa-exclamation-triangle"></i></span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render, redirect, reverse
-from .forms import OrderForm
+from django.conf import settings
 from django.contrib import messages
+
+from cart.contexts import cart_contents
+from .forms import OrderForm
+
+import stripe
 
 
 # Create your views here.
 def checkout(request):
     """Renders the checkout page"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your Shopping Cart is currently Empty")
         return redirect(reverse('products'))
+
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
     context = {
         'order_form': order_form,
-        'publishable_key': 'pk_test_51LcIJvIQDXjp8otbGZdOBifdz7sUeNxwfMkjkzndAxjjxOi5LZc9fsD9Uu5wSqBwd9OFIg35Zvr0APR2hg7e877X00LSVa7j7O',
-        'client_secret': 'client secret yeah long'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, 'checkout/checkout.html', context)
