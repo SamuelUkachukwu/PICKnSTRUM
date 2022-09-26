4475995ba2728a91bb50b788a6b946144ff486d7
SamuelUkachukwu <shybold@yahoo.com>
2022-09-26 22:46:45 +0000

    Added html and css elements to profile, updated profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 # Create your views here.
 def profile(request):
     """Renders the profile page"""
-    context = {}
+    profile = get_object_or_404(UserProfile, user=request.user)
+    context = {
+        'profile': profile
+    }
     return render(request, 'profiles/profile.html', context)
