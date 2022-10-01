bae6897892863bced47960ed984f884eac59d556
SamuelUkachukwu <shybold@yahoo.com>
2022-10-01 01:05:23 +0000

    Added the structure for the blog page

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog_home(request):
-    context = {}
+    context = {
+        'on_blog': True
+    }
     template_name = 'blog/blog_home.html'
     return render(request, template_name, context)
