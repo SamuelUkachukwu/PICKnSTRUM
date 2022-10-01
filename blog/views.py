93bd153c06d76626a7226e5cedfcb39de28aebd4
SamuelUkachukwu <shybold@yahoo.com>
2022-10-01 15:42:23 +0000

    Fixed issues with missing product image. Updated product fixture file. updated product view>

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from .models import Post, PostCategory
 
 
 # Create your views here.
 def blog_home(request):
+    posts = Post.objects.filter(status=1).order_by("-created_on")
+    top_stories = PostCategory.objects.filter(name='top_stories').order_by('?')
     context = {
-        'on_blog': True
+        'posts': posts,
+        'on_blog': True,
+        'top_stories': top_stories,
     }
     template_name = 'blog/blog_home.html'
     return render(request, template_name, context)
+
+
+def view_blog_article(request):
+    """View to view individual blog posts"""
+    pass
