8619ab9ed3f4de2c0b2a9823b07f87284ba7c7ab
SamuelUkachukwu <shybold@yahoo.com>
2022-10-02 18:48:41 +0000

    Update post-view template. Added like unlike functionality to post. Update hero image height. Changes to product card classes.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_home, name='blog_home'),
+    path('like/<slug:slug>/', views.PostLikes.as_view(), name='post_likes'),
+    path('dislike/<slug:slug>/', views.PostDislikes.as_view(), name='post_dislikes'),
     path('post/<slug:slug>/', views.view_blog_article, name='view_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.views import View
 from .models import Post, PostCategory
 
 
@@ -33,11 +34,69 @@ def view_blog_article(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     cat_list = PostCategory.objects.all()
 
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        liked = True
+
+    disliked = False
+    if post.dislikes.filter(id=request.user.id).exists():
+        disliked = True
     template_name = 'blog/post_view.html'
     context = {
         'post': post,
         'posts': posts,
         'cat_list': cat_list,
+        "liked": liked,
+        "disliked": disliked,
         'on_blog': True,
     }
     return render(request, template_name, context)
+
+
+class PostLikes(View):
+    """this view adds likes to a post
+    checks for dislikes and remove it"""
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        for dislikes in post.dislikes.all():
+            if dislikes == request.user:
+                post.dislikes.remove(request.user)
+                break
+
+        liked = False
+        for likes in post.likes.all():
+            if likes == request.user:
+                liked = True
+                break
+        if not liked:
+            post.likes.add(request.user)
+        if liked:
+            post.likes.remove(request.user)
+        return redirect(reverse('view_post', args=[slug]))
+
+
+
+class PostDislikes(View):
+    """this view adds dislikes to a post
+    checks for likes and remove it"""
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        for likes in post.likes.all():
+            if likes == request.user:
+                post.likes.remove(request.user)
+                break
+
+        disliked = False
+        for dislikes in post.dislikes.all():
+            if dislikes == request.user:
+                disliked = True
+                break
+        if not disliked:
+            post.dislikes.add(request.user)
+        if disliked:
+            post.dislikes.remove(request.user)
+        return redirect(reverse('view_post', args=[slug]))
