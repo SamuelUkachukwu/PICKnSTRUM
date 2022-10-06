f85bb15347c6c0a753ba48df40a6c0817141f060
SamuelUkachukwu <shybold@yahoo.com>
2022-10-06 14:56:52 +0000

    Added product/post add/edit elements to management

--- management/views.py
+++ management/views.py
@@ -1,7 +1,19 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from blog.models import Post
+from products.models import Product
 
 
 # Create your views here.
 def management(request):
+
+    paginator = Paginator(
+        Post.objects.all().order_by("-created_on"), 4)
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    context = {
+        'posts': posts,
+    }
     template_name = 'management/storekeep.html'
-    return render(request, template_name)
+    return render(request, template_name, context)
