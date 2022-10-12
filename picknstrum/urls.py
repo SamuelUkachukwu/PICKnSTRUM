dbfb3f97cbe908c602de6869cb712767cf1dad01
SamuelUkachukwu <shybold@yahoo.com>
2022-10-12 00:57:01 +0000

    Added 404 error handler.

--- picknstrum/urls.py
+++ picknstrum/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -30,3 +31,5 @@ urlpatterns = [
     path('management/', include('management.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'picknstrum.views.handler404'
